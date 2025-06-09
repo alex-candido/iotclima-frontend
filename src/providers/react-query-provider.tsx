0bb5ab11e9912ef55e350745408cbea22a1df413
alex-candido <alex.candido.tec@gmail.com>
2025-06-09 02:15:08 -0300

    created initial project structure

---
+++ src/providers/react-query-provider.tsx
@@ -0,0 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+
+const queryClient = new QueryClient();
+
+interface ReactQueryProviderProps {
+  children: React.ReactNode;
+}
+
+export function ReactQueryProvider({ children }: ReactQueryProviderProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
